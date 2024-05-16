03a88c9c31c529518d41df8b81d489c21a8dde82
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2024-05-16 17:42:33 +0200

    Añadido menú principal sin diseño.
    Tengo que arreglar el problema de las camaras. Se vuelven locas cuando hay dos personajes en la escena

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    public PhotonView playerPrefab; // Jugador que se va a instanciar 
-    public Transform spawnPoint;
+    public PhotonView _pj1Prefab, _pj2Prefab;
+    public Transform _spawnPointPj1, _spawnPointPj2;
 
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
     }
 
-
     public override void OnConnectedToMaster()
     {
         Debug.Log("Nos hemos conectado al master");
         PhotonNetwork.JoinRandomOrCreateRoom();
-
     }
+
     // Funcion que instancia el prefab de un jugador cuando se realiza una conexion al master
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+        if (SelectCharacterController.instance.isPj1Selected()) PhotonNetwork.Instantiate(_pj1Prefab.name, _spawnPointPj1.position, _spawnPointPj1.rotation);
+        else PhotonNetwork.Instantiate(_pj2Prefab.name, _spawnPointPj2.position, _spawnPointPj2.rotation); 
     }
 }
---
+++ Assets/Scripts/SelectCharacterController.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectCharacterController : MonoBehaviour
+{
+    public static SelectCharacterController instance;
+    [SerializeField] private bool isPj1 = false;
+
+    private void Awake()
+    {
+        if(SelectCharacterController.instance == null)
+        {
+            SelectCharacterController.instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnButtonPressed(bool isPj1)
+    {
+        this.isPj1 = isPj1;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public Boolean isPj1Selected() { return isPj1; }
+}
